4bc5ef3d89346d855a21a1385224d6fb186223ce
BuildTools <unconfigured@null.spigotmc.org>
2019-08-09 10:43:11 +0800

    bat脚本更新

---
+++ updata.bat
@@ -0,0 +1,5 @@
+java -cp Launch-Release-1.0.exe com.shuaijie.updata.IndexJar updata.bat Launch-Release-1.0.exe
+D:\GitHUB\Git\cmd\git.exe add .
+D:\GitHUB\Git\cmd\git.exe commit -m "bat½Å±¾¸üÐÂ"
+D:\GitHUB\Git\cmd\git.exe push
+pause
